f8ebb2870b50e3c08881372ac8180db731929ec4
ale4287 <alejandro.algarin@hotmail.com>
2023-06-28 04:31:02 -0300

    Creacion de ClienteController.py

---
+++ controller/ClienteController.py
@@ -0,0 +1,20 @@
+from dao.ClienteDao import ClienteDao
+from factory.ConnectionFactory import ConnectionFactory
+from modelo.Cliente import Cliente
+
+
+class ClienteController:
+    def __init__(self):
+        self.cliente_dao = ClienteDao(ConnectionFactory.get_connection('ClienteController'))
+
+    def guardar(self, cliente: Cliente):
+        self.cliente_dao.guardar(cliente)
+
+    def listar(self):
+        self.cliente_dao.listar()
+
+    def actualizar(self, cliente: Cliente):
+        self.cliente_dao.actualizar(cliente)
+
+    def eliminar(self, cliente: Cliente):
+        self.cliente_dao.eliminar(cliente)
