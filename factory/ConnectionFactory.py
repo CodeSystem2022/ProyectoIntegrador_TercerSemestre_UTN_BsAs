34b30f5432b418e9d323d82207d4deb93af290be
Maximiliano Chalin <mchalin@gmail.com>
2023-07-03 10:52:11 -0300

    Revision Connection Factory

--- factory/ConnectionFactory.py
+++ factory/ConnectionFactory.py
@@ -1,3 +1,5 @@
+import sys
+
 import psycopg2  # Librería para conectar con PostgreSQL
 
 
@@ -11,58 +13,73 @@ class ConnectionFactory:
     port = '5432'
 
     # Al inicializar una nueva conexion, la guarda en una lista
+
     def __init__(self):
         ConnectionFactory.listado_conexiones.append(self)
+        print("Se ha creado una nueva conexión.")
+        # Lista de conexiones
+        # for conexion in ConnectionFactory.listado_conexiones:
+        #     print(conexion)
+
+    def __str__(self):
+        return f"dbname={self.dbname} user={self.user} password={self.password} host={self.host} port={self.port}"
 
     # Chequea si existe la base de datos en la computadora
     # Si existe, no hace nada más, sino, la crea
     @staticmethod
     def chequearDB():
         dns = f"dbname='postgres' user={ConnectionFactory.user} password={ConnectionFactory.password} host={ConnectionFactory.host} port={ConnectionFactory.port}"
-        conn = psycopg2.connect(dns)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute("SELECT 1 FROM pg_database WHERE datname='ventas'")
-        exists = cursor.fetchone()
-        if not exists:
-            fd = open('sql/crear_base_de_datos.sql', 'r')
-            sqlFile = fd.read()
-            fd.close()
-            sqlCommands = sqlFile.split(';')
-            for command in sqlCommands:
-                print(command)
-                if command.strip() != '':
-                    cursor.execute(command)
-            conn.commit()
-            conn.close()
-            print("Base de datos creada.")
-            return False
-        else:
-            conn.close()
-            print("Base de datos ya existe.")
-            return True
+        conexion = psycopg2.connect(dns)
+        conexion.autocommit = True
+        with conexion.cursor() as cursor:
+            cursor.execute("SELECT 1 FROM pg_database WHERE datname='ventas'")
+            exists = cursor.fetchone()
+            if not exists:
+                with open('sql/crear_base_de_datos.sql', 'r') as fd:
+                    sqlFile = fd.read()
+
+                sqlCommands = sqlFile.split(';')
+                for command in sqlCommands:
+                    # print(command)
+                    if command.strip() != '':
+                        cursor.execute(command)
+                print("Base de datos creada.")
+                conexion.close()
+                return False
+            else:
+                conexion.close()
+                # print("Base de datos ya existe.")
+                return True
 
     # Si crea la base de datos, llama a la función para crear las tablas necesarias
     @staticmethod
     def crear_tablas():
-        conn = ConnectionFactory.get_connection('crear_tablas')
-        conn.autocommit = True
-        cursor = conn.cursor()
-        fd = open('sql/crear_tablas.sql', 'r')
-        sqlFile = fd.read()
-        fd.close()
-        sqlCommands = sqlFile.split(';')
-        for command in sqlCommands:
-            print(command)
-            if command.strip() != '':
-                cursor.execute(command)
-        conn.commit()
-        conn.close()
-        print("Tablas creadas.")
+        # Crea la conexión
+        conexion = ConnectionFactory.get_connection('crear_tablas')
+        # Activa el autocommit
+        conexion.autocommit = True
+        # Crea el cursor
+        with conexion.cursor() as cursor:
+            # Abre el archivo con los comandos sql
+            with open('sql/crear_tablas.sql', 'r') as fd:
+                # Lee el archivo y lo guarda en una variable
+                sqlFile = fd.read()
+
+            # Separa los comandos a ejecutar
+            sqlCommands = sqlFile.split(';')
+            # Recorre los comandos
+            for command in sqlCommands:
+                # print(command)
+                # Si el comando no está vacío, lo ejecuta
+                if command.strip() != '':
+                    cursor.execute(command)
+        # Cierra la conexión
+        conexion.close()
 
     # Este método devuelve una conexion creada
     @staticmethod
     def get_connection(desde_donde):
-        print(f'Creando conexión desde {desde_donde}...')
+        # TODO: Borrar print al terminar
+        print(f'Creando conexión desde {desde_donde}...', file=sys.stderr)
         dns = f'dbname={ConnectionFactory.dbname} user={ConnectionFactory.user} password={ConnectionFactory.password} host={ConnectionFactory.host} port={ConnectionFactory.port}'
         return psycopg2.connect(dns)
