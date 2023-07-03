a57fc5f86bb03d8363ca3b8db96386579a5242ca
Maximiliano Chalin <mchalin@gmail.com>
2023-07-03 10:55:29 -0300

    Revision Controllers

--- controller/ProductoController.py
+++ controller/ProductoController.py
@@ -3,18 +3,25 @@ from factory.ConnectionFactory import ConnectionFactory
 from modelo.Producto import Producto
 
 
+# Controlador de la tabla Productos
+# se encarga de comunicarse con la
+# base de datos y enviar la acción a realizar
+
 class ProductoController:
     def __init__(self):
-        self.producto_dao = ProductoDao(ConnectionFactory.get_connection('ProductoController'))
+        self.producto_dao = ProductoDao(ConnectionFactory().get_connection('ProductoController'))
 
     def guardar(self, producto: Producto):
         self.producto_dao.guardar(producto)
 
     def listar(self):
-        self.producto_dao.listar()
+        return self.producto_dao.listar()
 
     def actualizar(self, producto: Producto):
         self.producto_dao.actualizar(producto)
 
-    def eliminar(self, producto: Producto):
-        self.producto_dao.eliminar(producto)
+    def eliminar(self, id_producto: str):
+        return self.producto_dao.eliminar(id_producto)
+
+    def buscar_por_id(self, codigo_producto: str) -> Producto:
+        return self.producto_dao.buscar_por_id(codigo_producto)
--- controller/UsuarioController.py
+++ controller/UsuarioController.py
@@ -1,4 +1,3 @@
-
 from dao.UsuarioDao import UsuarioDao
 from factory.ConnectionFactory import ConnectionFactory
 from modelo.Usuario import Usuario
@@ -6,22 +5,19 @@ from modelo.Usuario import Usuario
 
 class UsuarioController:
     def __init__(self):
-        self.usuario_dao = UsuarioDao(ConnectionFactory.get_connection('UsuarioController'))
+        self.usuario_dao = UsuarioDao(ConnectionFactory().get_connection('UsuarioController'))
 
     def guardar(self, usuario: Usuario):
         self.usuario_dao.guardar(usuario)
 
     def listar(self):
-        self.usuario_dao.listar()
+        return self.usuario_dao.listar()
 
     def actualizar(self, usuario: Usuario):
         self.usuario_dao.actualizar(usuario)
 
-    def eliminar(self, usuario: Usuario):
-        self.usuario_dao.eliminar(usuario)
+    def eliminar(self, id_usuario: str):
+        return self.usuario_dao.eliminar(id_usuario)
 
-
-
-
-
-
+    def buscar_por_id(self, id_usuario: str):
+        return self.usuario_dao.buscar_por_id(id_usuario)
